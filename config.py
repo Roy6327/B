6dc6931fb181255bd33aa7b13063e600819d1274
Roy6327 <141469948+Roy6327@users.noreply.github.com>
2023-09-02 14:39:17 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
 
     CHANNEL_ACCESS_TOKEN = 'lzozWfpuVLXc+8HXi2P0Fx8mKEHwzL/qNByk9e18l1iNH/gKdPGzz9CeBReDtT5pn1ykwoheynpAhlyatxi8ZTbGkBWBKzeKQgnfMdDkO4SZrqM7r4IQBuK04XDjyBW+MYr6eWixavkESb6/mwKKlwdB04t89/1O/w1cDnyilFU='
     CHANNEL_SECRET = '1768601e8a345509555bc9999ca777ba'
-    LINE_PAY_ID = 'test_202309028382@line.pay'
-    LINE_PAY_SECRET = "'kzH1xh7T1"
+    LINE_PAY_ID = '2000604812'
+    LINE_PAY_SECRET = "879668915dfffad250c16f2349c279f0"
     STORE_IMAGE_URL = 'https://i.imgur.com/0KinQXT.jpg'
