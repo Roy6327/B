c8dd03f2f8c31e71e8e17f2012bb4935d38d46c0
Roy6327 <141469948+Roy6327@users.noreply.github.com>
2023-09-16 14:21:09 +0800

    Add files via upload

--- line_bot_api.py
+++ line_bot_api.py
@@ -13,6 +13,6 @@ from linebot.models import (
 )
 
 # Channel access token
-line_bot_api = LineBotApi('tHtDbQSQJjIZcgHRRkgXrgj0CPLVeKMHZNk6akbQJCVvnLRmPf0TiQF/L/CXZ6+ItpUKaG+Yh1Z8YmEFDrWg+lXBavoEw8YXvhsX6dysouoeJZUbHnkdBzekZSl+c535b21o0pvCNqv/UeD7EYR37QdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('lzozWfpuVLXc+8HXi2P0Fx8mKEHwzL/qNByk9e18l1iNH/gKdPGzz9CeBReDtT5pn1ykwoheynpAhlyatxi8ZTbGkBWBKzeKQgnfMdDkO4SZrqM7r4IQBuK04XDjyBW+MYr6eWixavkESb6/mwKKlwdB04t89/1O/w1cDnyilFU=')
 #Channel secret
-handler = WebhookHandler('9b896be843e2ba632a0f456162a8585e')
+handler = WebhookHandler('1768601e8a345509555bc9999ca777ba')
